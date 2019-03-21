a9e0631d42394300b9696b926bf49655862c0ff7
ghoulich <ghoulich@163.com>
2019-03-21 09:05:12 +0800

    修正bug

--- build.docker.sh
+++ build.docker.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
 cd amd64
-sudo docker build -t bestwu/deepin .
-sudo docker tag bestwu/deepin bestwu/deepin:panda
-sudo docker tag bestwu/deepin bestwu/deepin:15.7
+sudo docker build -t ghoulich-deepin/deepin-amd64:latest .
+docker tag ghoulich-deepin/deepin-amd64:latest registry.cn-hangzhou.aliyuncs.com/ghoulich-deepin/deepin-amd64:15.9.2
+docker tag ghoulich-deepin/deepin-amd64:latest registry.cn-hangzhou.aliyuncs.com/ghoulich-deepin/deepin-amd64:latest
+docker push registry.cn-hangzhou.aliyuncs.com/ghoulich-deepin/deepin-amd64:15.9.2
+docker push registry.cn-hangzhou.aliyuncs.com/ghoulich-deepin/deepin-amd64:latest
 cd ..
+
 cd i386
-sudo docker build -t bestwu/deepin:i386 .
-sudo docker tag bestwu/deepin:i386 bestwu/deepin:panda-i386
-sudo docker tag bestwu/deepin:i386 bestwu/deepin:15.7-i386
+sudo docker build -t ghoulich-deepin/deepin-i386:latest .
+docker tag ghoulich-deepin/deepin-i386:latest registry.cn-hangzhou.aliyuncs.com/ghoulich-deepin/deepin-i386:15.9.2
+docker tag ghoulich-deepin/deepin-i386:latest registry.cn-hangzhou.aliyuncs.com/ghoulich-deepin/deepin-i386:latest
+docker push registry.cn-hangzhou.aliyuncs.com/ghoulich-deepin/deepin-i386:15.9.2
+docker push registry.cn-hangzhou.aliyuncs.com/ghoulich-deepin/deepin-i386:latest
 cd ..
-sudo docker push bestwu/deepin
